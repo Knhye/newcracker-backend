f0b95b15b2614c41e4ffad4af4f4997d3830b145
Knhye <knahye311@gmail.com>
2025-12-24 12:14:01 +0900

    fix: 헬퍼 함수 의존성 주입

--- src/main/java/com/example/newcracker/common/helper/UserHelper.java
+++ src/main/java/com/example/newcracker/common/helper/UserHelper.java
@@ -6,16 +6,19 @@ import com.example.newcracker.common.exception.ResourceNotFoundException;
 import com.example.newcracker.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 @RequiredArgsConstructor
 public class UserHelper {
     private final UserRepository userRepository;
-    public Users extractUser(){
+
+    public Users extractUser() {
         String email = SecurityContextHolder.getContext()
                 .getAuthentication()
                 .getName();
 
-        if(email == null){
+        if (email == null) {
             throw new NotLoggedInException("로그인 해주세요.");
         }
 
