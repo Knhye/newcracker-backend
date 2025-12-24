28709c120d06b495627bb29906821432c76cf8e6
Knhye <knahye311@gmail.com>
2025-12-24 12:06:12 +0900

    fix: 잘못된 import 제거

--- src/main/java/com/example/newcracker/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/newcracker/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.newcracker.common.exception;
 
 import com.example.newcracker.common.response.ApiResponse;
-import com.example.newcracker.global.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
